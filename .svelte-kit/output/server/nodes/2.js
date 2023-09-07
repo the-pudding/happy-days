79bbb6827bba86ab5315e761bb3ff0a905864db0
alvinschang <alvinschang@gmail.com>
2023-09-07 15:58:42 -0400

    update github pages

--- .svelte-kit/output/server/nodes/2.js
+++ .svelte-kit/output/server/nodes/2.js
@@ -5,6 +5,6 @@ let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
 export { server };
 export const server_id = "src/routes/+page.server.js";
-export const imports = ["_app/immutable/nodes/2.b742902e.js","_app/immutable/chunks/scheduler.ececd849.js","_app/immutable/chunks/index.86c39e87.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/wordmark.fc3f3709.js"];
-export const stylesheets = ["_app/immutable/assets/2.2a75d543.css"];
+export const imports = ["_app/immutable/nodes/2.ab2c6bca.js","_app/immutable/chunks/scheduler.ececd849.js","_app/immutable/chunks/index.86c39e87.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/wordmark.fc3f3709.js"];
+export const stylesheets = ["_app/immutable/assets/2.1bb3c866.css"];
 export const fonts = [];
--- .svelte-kit/output/server/nodes/4.js
+++ .svelte-kit/output/server/nodes/4.js
@@ -3,6 +3,6 @@
 export const index = 4;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/demo/fonts/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.88556d4f.js","_app/immutable/chunks/scheduler.ececd849.js","_app/immutable/chunks/index.86c39e87.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/paths.9b7ce5f7.js"];
+export const imports = ["_app/immutable/nodes/4.de3d3009.js","_app/immutable/chunks/scheduler.ececd849.js","_app/immutable/chunks/index.86c39e87.js","_app/immutable/chunks/each.e59479a4.js","_app/immutable/chunks/paths.d3d011e8.js"];
 export const stylesheets = ["_app/immutable/assets/4.694798f8.css"];
 export const fonts = [];
